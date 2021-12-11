b06ebd5c5c0dae6e70e79d1175434f9c757de0dc
talhabinhasan <77718027+talhabinhasan@users.noreply.github.com>
2021-12-11 23:08:36 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const nav = document.querySelector('#nav');
+let navTop = nav.offsetTop;
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+function stickyNav() {
+    if (window.scrollY > 0) {    
+      nav.classList.add('sticky');
+    } else {
+      nav.classList.remove('sticky');    
+    }
+  }
+  
+  window.addEventListener('scroll', stickyNav);
