aa0ad1846b8983cd3577b1922ab918582f56c67f
talhabinhasan <77718027+talhabinhasan@users.noreply.github.com>
2021-12-13 00:02:49 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const firstSection = document.querySelector("section:first-of-type")
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
+    firstSection.classList.toggle('marginTop');
 })
+
+window.addEventListener('scroll', function(){ 
+
+    if(window.scrollY > 100){
+        firstSection.classList.add("scrolled");
+    }
+    else {
+        firstSection.classList.remove("scrolled");
+    }
+
+});
